8c2d9cb93ea98f0d160c7b7674fba7918a66ef5f
Anthony Arana <anthony.arana@tecsup.edu.pe>
2025-11-11 20:32:24 -0500

    feat: agregar modelos Nota y Recordatorio con relaciones

--- app/Models/Recordatorio.php
+++ app/Models/Recordatorio.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recordatorio extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nota_id', 'fecha_vencimiento', 'completado'];
+
+    public function nota()
+    {
+        return $this->belongsTo(Nota::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function notas()
+    {
+        return $this->hasMany(Nota::class);
+    }
 }
