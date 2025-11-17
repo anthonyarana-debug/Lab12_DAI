4fa64616b61967ab6569a2eb58f7fe14d7f9de9b
Anthony Arana <anthony.arana@tecsup.edu.pe>
2025-11-17 17:59:43 -0500

    feat: definir estructura de tabla actividades y relación con notas

--- database/migrations/2025_11_17_225300_create_actividads_table.php
+++ database/migrations/2025_11_17_225300_create_actividads_table.php
@@ -6,22 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('actividads', function (Blueprint $table) {
+        Schema::create('actividades', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('nota_id')->constrained()->onDelete('cascade'); // FK a notas
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+            $table->boolean('completada')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('actividads');
+        Schema::dropIfExists('actividades');
     }
 };
